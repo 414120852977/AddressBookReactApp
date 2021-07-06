6d4354ae4448fcbf872f09077d69806a0867047b
Ashok Mane <ashokmane81800@gmail.com>
2021-07-06 16:19:07 +0530

    [ashok] added user interface for an addressbook app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router , Switch, Route} from "react-router-dom";
+import AddressBookForm from '../src/component/addressbook-form/addressbook-form';
 
 function App() {
   return (
     <div className="App">
-      <h1><marquee>Welcome To AddressBook</marquee></h1>
+      <Router>
+        <Switch>
+          <Route  path = "">
+          <AddressBookForm />
+            </Route>
+          </Switch>
+        </Router>
     </div>
   );
 }
